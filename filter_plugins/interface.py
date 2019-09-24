84ec3208aa42ecf0749f4a1233b9644f800900c0
Tobias Schramm <tobleminer@gmail.com>
2019-09-25 01:25:00 +0200

    filter_plugins: Add inteface_has_master filter

--- filter_plugins/interface.py
+++ filter_plugins/interface.py
@@ -24,22 +24,36 @@ def set_home_flag(addr):
     addr['flags'].append('home')
   return addr
 
-def mtu_fixup(interface, ifname, interfaces):
-  for iface in interfaces:
+def get_master(ifname, interfaces):
+  for master_name in interfaces.keys():
+    iface = interfaces[master_name]
     if not 'managed' in iface or not iface['managed']:
-      continue
-    if iface['type'] != 'bridge':
-      continue
-    if not mtu in iface:
       continue
     if not 'slaves' in iface:
       continue
     slaves = iface['slaves'] if isinstance(iface['slaves'], list) else [ iface['slaves'] ]
     if not ifname in slaves:
       continue
-    interface['mtu'] = iface['mtu']
-    break
+    return master_name
 
+  return None
+
+def has_master(ifname, interfaces):
+  return get_master(ifname, interfaces) != None
+
+def mtu_fixup(interface, ifname, interfaces):
+  if not has_master(ifname, interfaces):
+    return interface
+
+  master_name = get_master(ifname, interfaces)
+  master = interfaces[master_name]
+
+  if master['type'] != 'bridge':
+    return interface
+  if not mtu in master:
+    return interface
+
+  interface['mtu'] = master['mtu']
   return interface
 
 class FilterModule(object):
@@ -48,4 +62,5 @@ class FilterModule(object):
       'interface_normalize_addresses': normalize_addresses,
       'interface_set_home_flag': set_home_flag,
       'interface_mtu_fixup': mtu_fixup,
+      'interface_has_master': has_master
     }
