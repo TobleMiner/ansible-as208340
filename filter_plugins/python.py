55c981e1e228dfd58a77d2314cf13118f7731b90
Tobias Schramm <tobleminer@gmail.com>
2019-09-26 22:18:38 +0200

    filter_plugins: Add dict2tuples filter

--- filter_plugins/python.py
+++ filter_plugins/python.py
@@ -3,8 +3,12 @@ import functools
 def python_filter(data, exp):
   return eval(exp)
 
+def dict2tuples(d):
+  return d.items()
+
 class FilterModule(object):
   def filters(self):
     return {
-      'python': python_filter
+      'python': python_filter,
+      'dict2tuples': dict2tuples
     }
